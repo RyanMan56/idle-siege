7d9a42f30dd3e584592b7f3366bcac307e07cfe5
Ryan Jones <RyanMan56@gmail.com>
2021-01-09 22:59:33 +0000

    Y Axis of GraphRenderer, LOGARITHM scale, text alignment

--- core/src/com/rbj_games/idle_siege/TextDrawable.java
+++ core/src/com/rbj_games/idle_siege/TextDrawable.java
@@ -12,11 +12,22 @@ public class TextDrawable implements IDrawable {
 	BitmapFont font;
 	GlyphLayout layout;
 	Vector3 textPos;
+	Align xAlign, yAlign;
+
+	public enum Align {
+		START, CENTER, END
+	}
+
+	public enum Axis {
+		X, Y
+	}
 	
-	public TextDrawable(IdleSiege game, Vector2 worldPos, String text) {
+	public TextDrawable(IdleSiege game, Vector2 worldPos, String text, Align xAlign, Align yAlign) {
 		this.game = game;
 		this.worldPos = worldPos;
 		this.text = text;
+		this.xAlign = xAlign;
+		this.yAlign = yAlign;
 		
 		font = game.assetManager.get("OpenSans-Light_Small.ttf", BitmapFont.class);
 		layout = new GlyphLayout(font, text);
@@ -25,9 +36,20 @@ public class TextDrawable implements IDrawable {
 		textPos = game.textCamera.unproject(screenPos);
 	}
 
+	public float calculatePosition(Align align, float pos, float size, Axis axis) {
+		if (align == Align.CENTER) {
+			return axis == Axis.X ? pos - size / 2 : pos + size / 2;
+		}
+		if (align == Align.END) {
+			return pos;
+		}
+		return axis == Axis.X ? pos - size : pos + size / 2; // Align.START
+	}
+
 	@Override
 	public void draw() {
-		font.draw(game.batch, layout, textPos.x - layout.width / 2, textPos.y + layout.height / 2);
+//		font.draw(game.batch, layout, textPos.x - layout.width / 2, textPos.y + layout.height / 2);
+		font.draw(game.batch, layout, calculatePosition(xAlign, textPos.x, layout.width, Axis.X), calculatePosition(yAlign, textPos.y, layout.height, Axis.Y));
 	}
 	
 	public void resize() {
--- core/src/com/rbj_games/idle_siege/utils/GraphRenderer.java
+++ core/src/com/rbj_games/idle_siege/utils/GraphRenderer.java
@@ -14,19 +14,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.rbj_games.idle_siege.IDrawable;
 import com.rbj_games.idle_siege.IdleSiege;
 import com.rbj_games.idle_siege.TextDrawable;
-
-//class GraphLabel {
-//	Vector2 position;
-//	TextDrawable textDrawable;
-//	
-//	public GraphLabel(Vector2 position, TextDrawable textDrawable) {
-//		this.position = position;
-//		this.textDrawable = textDrawable;
-//	}
-//	
-//	// Override hashCode function if HashMap needs accessing from more than just the object instance
-//	// Default hashCode implementation converts memory address of object to an integer
-//}
+import com.rbj_games.idle_siege.TextDrawable.Align;
 
 // If ScaleType is LINEAR_MINUTES then all values are in seconds, but displayed in minutes on the graph
 public class GraphRenderer {
@@ -36,7 +24,7 @@ public class GraphRenderer {
 	private ScaleType scaleTypeX, scaleTypeY;
 	private ShapeRenderer shapeRenderer;
 	private Vector2 labelGap;
-	private TextDrawable[] labelsX;
+	private TextDrawable[] labelsX, labelsY;
 	BitmapFont font;
 	FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/open_sans/OpenSans-Light.ttf"));
 	FreeTypeFontGenerator.FreeTypeFontParameter fontParameter;
@@ -44,6 +32,7 @@ public class GraphRenderer {
 	
 	// In the case of a log scale, the intervals are taken at every processed log.
 	// e.g. interval = 1. 10 ln = 1, 100 ln = 2. 10, 100 get displayed on the axis.
+	// 10^1 = 10, 10^2 = 100
 	public GraphRenderer(IdleSiege game, Map<IDrawable, IDrawable> textDrawables, Vector2 position, Vector2 size, Vector2 rangeX, Vector2 rangeY, Vector2 intervals, ScaleType scaleTypeX, ScaleType scaleTypeY) {
 		this.game = game;
 		this.textDrawables = textDrawables;
@@ -51,28 +40,51 @@ public class GraphRenderer {
 		this.size = size;
 		this.rangeX = scaleTypeX == ScaleType.LINEAR_MINUTES ? rangeX.scl(60f) : rangeX;
 		this.rangeY = rangeY;
-		this.intervals = new Vector2(scaleTypeX == ScaleType.LINEAR_MINUTES ? intervals.x * 60f : intervals.x, intervals.y);
+		this.intervals = new Vector2(scaleIntervals(scaleTypeX, intervals.y), scaleIntervals(scaleTypeY, intervals.y));
 		this.scaleTypeX = scaleTypeX;
 		this.scaleTypeY = scaleTypeY;
 		shapeRenderer = new ShapeRenderer();
 		labelGap = new Vector2(10f, 3f);
-		
-		setupXAxisLabels();
+
+		setupLabels(this.rangeX, this.intervals.x, this.size.x, this.scaleTypeX, "x");
+		setupLabels(this.rangeY, this.intervals.y, this.size.y, this.scaleTypeY, "y");
+
+		System.out.println(this.intervals);
 	}
-	
-	private void setupXAxisLabels() {
-		// range - x: from, y: to
-		int labelCount = (int) Math.round((rangeX.y - rangeX.x) / intervals.x) + 1;
-		float ratio = 1f / (labelCount - 1);
-		labelGap.x = size.x * ratio;
-		labelsX = new TextDrawable[labelCount];
+
+	private float scaleIntervals(ScaleType scaleType, float interval) {
+		if (scaleType == ScaleType.LINEAR_MINUTES) {
+			return interval * 60f;
+		}
+		return interval; // ScaleType.LINEAR
+	}
+
+	private float scaleValue(ScaleType scaleType, float value) {
+		if (scaleType == ScaleType.LINEAR_MINUTES) {
+			return value / 60f;
+		}
+		if (scaleType == ScaleType.LOGARITHM) {
+			return (float) Math.pow(10f, value);
+		}
+		return value; // ScaleType.LINEAR
+	}
+
+	private void setupLabels(Vector2 range, float interval, float size, ScaleType scaleType, String axis) {
+		int labelCount = (int) Math.round((range.y - range.x) / interval) + 1;
 		for (int i = 0; i < labelCount; i++) {
-			float labelValue = rangeX.x + intervals.x * i;
-			float labelText = scaleTypeX == ScaleType.LINEAR_MINUTES ? labelValue / 60f : labelValue;
-			float posX = Utils.ConvertRanges(rangeX.y, rangeX.x, position.x + size.x, position.x, labelValue);
-			labelsX[i] = new TextDrawable(game, new Vector2(posX, position.y - 2f), ""+labelText);
-//			labelsX[i] = new GraphLabel(new Vector2(position.x + (labelGap.x * i), position.y), ""+i); // TODO: calculate what the actual label value is. Or use the label value to calculate the correct offset
-			textDrawables.put(labelsX[i], labelsX[i]);
+			float labelValue = range.x + interval * i;
+			float labelText = scaleValue(scaleType, labelValue);
+			if (axis == "x") {
+				float posX = Utils.ConvertRanges(range.y, range.x, position.x + size, position.x, labelValue);
+				labelsX = new TextDrawable[labelCount];
+				labelsX[i] = new TextDrawable(game, new Vector2(posX, position.y - 3f), ""+labelText, Align.CENTER, Align.CENTER);
+				textDrawables.put(labelsX[i], labelsX[i]);
+			} else {
+				float posY = Utils.ConvertRanges(range.y, range.x, position.y + size, position.y, labelValue);
+				labelsY = new TextDrawable[labelCount];
+				labelsY[i] = new TextDrawable(game, new Vector2(position.x - 3f, posY), ""+labelText, Align.START, Align.CENTER);
+				textDrawables.put(labelsY[i], labelsY[i]);
+			}
 		}
 	}
 	
